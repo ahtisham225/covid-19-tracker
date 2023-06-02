f30c91bf44b54dbac0f05e6d6eb1a8ed64a88a59
Ahtisham <ahtisham225ali@gmail.com>
2023-06-02 12:22:53 +0500

    Structuring the Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build Covid 19 Tracker</h1>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
+      {/* Footer */}
     </div>
   );
 }
