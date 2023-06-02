727719300fb73274516f80d8e26adc9c0bacd8dd
Ahtisham <ahtisham225ali@gmail.com>
2023-06-02 12:52:45 +0500

    Header and DropDown Flex Box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React,  {useState} from 'react';
 import './App.css';
-
+import { MenuItem, FormControl, Select } from "@mui/material";
 function App() {
+
+  const [countries, setCountries] = useState(['Pakistan', 'USA']);
   return (
     <div className="app">
-      <h1>Let's build Covid 19 Tracker</h1>
+      
+      
       {/* Header */}
+      <div className="app__header">
+      <h1>Covid 19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          {/* Loop through all the countries and show a drop down list of the options */}
+          {
+            countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+          }
+        </Select>  
+      </FormControl>
+      </div>
       {/* Title + Select input dropdown field */}
       {/* InfoBox */}
       {/* InfoBox */}
